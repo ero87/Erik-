6ceffda4e2889e4e99a49425abc8cdf1e272182f
Arman Vardanyan <arman5991@mail.ru>
2018-06-20 20:27:39 +0400

    make code for button 1\

--- App/app/src/main/java/vtc/room/a101/app/MainActivity.java
+++ App/app/src/main/java/vtc/room/a101/app/MainActivity.java
@@ -2,13 +2,25 @@ package vtc.room.a101.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button levon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        levon = (Button) findViewById(R.id.button1);
+        levon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.levon, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 
 }
